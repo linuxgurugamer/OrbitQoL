31b4bb3b9a214ac39facb0dae2600d424110de7e
James Glaze <jamespglaze@gmail.com>
2024-11-10 21:22:20 -0600

    Inital commit

---
+++ OrbitQoL/OrbitQoLInjector.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using UnityEngine;
+using System.Reflection;
+using HarmonyLib;
+using KSP.UI.Screens.Mapview;
+
+namespace OrbitQoLInjector
+{
+
+    [KSPAddon(KSPAddon.Startup.Flight, true)]
+    public class OrbitQoLInjector : MonoBehaviour
+    {
+        public void Awake()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Harmony harmony = new Harmony("CameraInjector");
+            harmony.PatchAll(assembly);
+        }
+    }
+
+    [HarmonyPatch(typeof(PatchRendering), "mnEnd_OnUpdateCaption", new Type[] { typeof(MapNode), typeof(MapNode.CaptionData) })]
+    public static class mnEnd_OnUpdateCaptionOverride
+    {
+        private static void Postfix(PatchRendering __instance, ref MapNode n, ref MapNode.CaptionData cData)
+        {
+            switch (__instance.patch.patchEndTransition)
+            {
+                case Orbit.PatchTransitionType.ENCOUNTER:
+                    Orbit nextPatch = __instance.patch.nextPatch;
+                    cData.captionLine2 = "Rel-V: " + nextPatch.getOrbitalSpeedAt(nextPatch.StartUT).ToString("0.##") + "m/s";
+                    break;
+                case Orbit.PatchTransitionType.ESCAPE:
+                    cData.captionLine2 = "Exit-V: " + __instance.patch.getOrbitalSpeedAt(__instance.patch.EndUT).ToString("0.##") + "m/s";
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(OrbitTargeter), "DropInvalidTargets")]
+    public static class DropInvalidTargetsOverride
+    {
+        private static bool Prefix(ref bool __result)
+        {
+            __result = false;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(FlightGlobals), "UpdateInformation", new Type[] { typeof(bool) })]
+    public static class UpdateInformationOverride
+    {
+        static ITargetable target;
+        private static void Prefix(ref FlightGlobals __instance, ref bool fixedUpdate)
+        {
+            target = __instance.VesselTarget ?? null;
+        }
+        private static void Postfix(ref FlightGlobals __instance, ref bool fixedUpdate)
+        {
+            if (__instance.VesselTarget == null && target != null)
+            {
+                __instance.SetVesselTarget(target);
+                if (__instance.VesselTarget != null)
+                {
+                    if (__instance.VesselTarget.GetTransform() == null)
+                    {
+                        __instance.SetVesselTarget(null);
+                    }
+                    else if (!__instance.VesselTarget.GetActiveTargetable())
+                    {
+                        if (__instance.VesselTarget.GetVessel() == __instance.activeVessel)
+                        {
+                            __instance.SetVesselTarget(null);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ OrbitQoL/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OrbitQoL")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OrbitQoL")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b5cd2ba8-364d-4e18-8389-30651a821a71")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: KSPAssemblyDependency("HarmonyKSP", 1, 0)]
