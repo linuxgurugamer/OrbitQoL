f337760a908a79c846a8fcc1e2f11619782ffdac
linuxgurugamer@gmail.com <linuxgurugamer@gmail.com>
2025-02-03 12:46:13 -0500

    minor local changes

--- buildRelease.bat
+++ buildRelease.bat
@@ -58,7 +58,7 @@ echo Version:  %VERSION%
 rem Build the zip FILE
 cd %GAMEDATA%\..
 
-set FILE="%RELEASEDIR%\%GAMEDIR%-%VERSION%.zip"
+set FILE="%RELEASEDIR%\OrbitQoL-%VERSION%.zip"
 IF EXIST %FILE% del /F %FILE%
 %ZIP% a -tzip %FILE% GameData
 
